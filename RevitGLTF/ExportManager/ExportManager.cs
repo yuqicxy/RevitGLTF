f29791f7f39da349a7dcc01c350fbe8c5ad7b1ba
Qi Yu <yuqicxy@users.noreply.github.com>
2020-03-10 20:12:40 +0800

    修改BabylonGLTF以支持实例化导出GLTF

--- RevitGLTF/ExportManager/ExportManager.cs
+++ RevitGLTF/ExportManager/ExportManager.cs
@@ -32,30 +32,12 @@ namespace RevitGLTF
             {
                 exporter.Export(exportableView);
             }
-            //catch(System.IO.IOException exception)//处理 I / O 错误。
-            //{
-            //
-            //}
-            //catch(System.IndexOutOfRangeException exception)//处理当方法指向超出范围的数组索引时生成的错误。
-            //{
-            //}
-            //catch(System.ArrayTypeMismatchException exception)//处理当数组类型不匹配时生成的错误。
-            //{ }
-            //catch(System.NullReferenceException exception) //处理当依从一个空对象时生成的错误。
-            //{ }
-            //catch(System.DivideByZeroException exception) //处理当除以零时生成的错误。
-            //{ }
-            //catch(System.InvalidCastException exception) //处理在类型转换期间生成的错误。
-            //{ }
-            //catch(System.OutOfMemoryException exception) //处理空闲内存不足生成的错误。
-            //{ }
-            //catch(System.StackOverflowException exception) //处理栈溢出生成的错误。
-            //{ }
             catch(System.Exception exception)
             {
                 TaskDialog dialog = new TaskDialog("exception");
                 dialog.MainContent = exception.Message;
             }
+
         }
 
         private ExportConfig mExportConfig;
---
+++ RevitGLTF/ExportManager/GLTFExportManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Babylon2GLTF;
+using BabylonExport.Entities;
+using Utilities;
+
+using Autodesk.Revit.DB;
+
+namespace RevitGLTF
+{
+    class GLTFExportManager : ILoggingProvider
+    {
+        private ExportConfig mConfig = null;
+        private ExportParameters mExportParameters = null;
+
+        public BabylonScene Scene { get; set; }
+        public GLTFExportManager(ExportConfig config)
+        {
+            mConfig = config;
+            mExportParameters = new ExportParameters();
+            mExportParameters.dracoCompression = true;
+            mExportParameters.optimizeVertices = true;
+
+            Scene = new BabylonScene(mConfig.mOutPutPath);
+            Scene.producer = new BabylonProducer { name = "Revit", version = "2020" };
+        }
+
+        public void Export()
+        {
+            Export(Scene);
+        }
+
+        public void Export(BabylonScene scene)
+        {
+            try 
+            {
+                scene.Prepare(false, false);
+                GLTFExporter gltfExporter = new GLTFExporter();
+                gltfExporter.ExportGltf(mExportParameters, scene, mConfig.mOutPutPath, mConfig.mOutputFilename + mConfig.mOutputFormat, false, this);
+            }
+            catch(Exception exp)
+            {
+                log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+                log.Error(exp.Source);
+                log.Error(exp.TargetSite.ToString());
+                log.Error(exp.Message);
+                log.Error(exp.StackTrace);
+            }
+            
+        }
+
+        public void CheckCancelled()
+        {
+        }
+
+        public void RaiseError(string error, int rank = 0)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            log.Error(error);
+        }
+
+        public void RaiseMessage(string message, int rank = 0, bool emphasis = false)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            log.Info(message);
+        }
+
+        public void RaiseMessage(string message, System.Drawing.Color color, int rank = 0, bool emphasis = false)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            log.Info(message);
+        }
+
+        public void RaiseVerbose(string message, int rank = 0, bool emphasis = false)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            log.Info(message);
+        }
+
+        public void RaiseWarning(string warning, int rank = 0)
+        {
+            log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            log.Warn(warning);
+        }
+
+        public void ReportProgressChanged(int progress)
+        {
+        }
+    }
+}
