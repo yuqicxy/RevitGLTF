eff37713f38d22fea0ab3e11a428ef74a9744bca
yuqicxy <yuqicxy@foxmail.com>
2020-03-25 17:35:46 +0800

    更新测试项目配置

--- Test/TestNVTT/Program.cs
+++ Test/TestNVTT/Program.cs
@@ -8,8 +8,8 @@ namespace TestNVTT
         static void Main(string[] args)
         {
             //ToNormal
-            String input = "E:\\RevitPlugin\\RevitGLTF\\Test\\TestNVTT\\bin\\Release\\ceiling_tile_dots_narrow_gap_taper_600x600mm_bump.jpg";
-            String output = "E:\\RevitPlugin\\RevitGLTF\\Test\\TestNVTT\\bin\\Release\\ceiling_tile_dots_narrow_gap_taper_600x600mm_bump.png";
+            String input = "F:\\RevitPlugin\\RevitGLTF\\Test\\TestNVTT\\bin\\Release\\netcoreapp3.1\\brick_bump.jpg";
+            String output = "F:\\RevitPlugin\\RevitGLTF\\Test\\TestNVTT\\bin\\Release\\netcoreapp3.1\\brick_bump.png";
             Compressor nvttCompressor = new Compressor();
             nvttCompressor.BumpMapToNormalMap(input, output);
         }
